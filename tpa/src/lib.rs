f85507713ab13998b38a343c948df544a49a9d49
Chris <chris.clark@coffeeb4code.com>
2021-12-05 17:06:03 -0600

    need to push forward characters for multi character parsing

--- tpa/src/lib.rs
+++ tpa/src/lib.rs
@@ -3,114 +3,27 @@
 pub struct TPAError(String);
 
 const TYPES_L: &'static [&'static str] = &[
-    "i32",
-    "u32",
-    "i64",
-    "i16",
-    "u16",
-    "u8",
-    "i8",
-    "bit",
-    "f64",
-    "f32",
-    "fn",
-    "null",
-    "char",
-    "string",
-    "box",
-    "ptr",
-    "addr",
-    "list",
+    "i32", "u32", "i64", "i16", "u16", "u8", "i8", "bit", "f64", "f32", "fn", "null", "char",
+    "string", "box", "ptr", "addr", "list",
 ];
 
 const KEYWORDS_L: &'static [&'static str] = &[
-    "mut",
-    "const",
-    "i32",
-    "u32",
-    "i64",
-    "i16",
-    "u16",
-    "u8",
-    "i8",
-    "bit",
-    "f64",
-    "f32",
-    "fn",
-    "if",
-    "else",
-    "type",
-    "this",
-    "null",
-    "undef",
-    "char",
-    "string",
-    "inline",
-    "static",
-    "switch",
-    "for",
-    "in",
-    "of",
-    "break",
-    "enum",
-    "pub",
-    "return",
-    "async",
-    "await",
-    "box",
-    "trait",
-    "ptr",
-    "match",
-    "addr",
-    "list",
-    "vol",
-    "true",
-    "false",
+    "mut", "const", "i32", "u32", "i64", "i16", "u16", "u8", "i8", "bit", "f64", "f32", "fn", "if",
+    "else", "type", "this", "null", "undef", "char", "string", "inline", "static", "switch", "for",
+    "in", "of", "break", "enum", "pub", "return", "async", "await", "box", "trait", "ptr", "match",
+    "addr", "list", "vol", "true", "false",
 ];
 
-const KEYWORDS_SIZE_L: &'static [u8] = &[
-    3,
-    5,
-    3,
-    3, // u32
-    3,
-    3,
-    3,
-    2,
-    2,
-    3, // bit
-    3,
-    3,
-    2,
-    2, // if
-    4,
-    4,
-    4,
-    4,
-    5, // undef
-    4,
-    6, // string
-    6,
-    6,
-    6, // switch
-    3,
-    2,
-    2,
-    5, // break
-    4,
-    3,
-    6,
-    5,
-    5,
-    3,
-    5,
-    3,
-    5, // match
-    4,
-    4,
-    3,
-    4,
-    5
+const KEYWORDS_SIZE_L: &'static [usize] = &[
+    3, 5, 3, 3, // u32
+    3, 3, 3, 2, 2, 3, // bit
+    3, 3, 2, 2, // if
+    4, 4, 4, 4, 5, // undef
+    4, 6, // string
+    6, 6, 6, // switch
+    3, 2, 2, 5, // break
+    4, 3, 6, 5, 5, 3, 5, 3, 5, // match
+    4, 4, 3, 4, 5,
 ];
 
 #[derive(Debug, PartialEq)]
@@ -155,47 +68,100 @@ pub enum KEYWORDS {
     ADDR,
     VOL,
     LIST,
+    TRUE,
+    FALSE,
 }
 
+impl KEYWORDS {
+    fn from_usize(value: usize) -> KEYWORDS {
+        match value {
+            0 => KEYWORDS::MUT,
+            1 => KEYWORDS::CONST,
+            2 => KEYWORDS::I32,
+            3 => KEYWORDS::U32,
+            4 => KEYWORDS::I64,
+            5 => KEYWORDS::I16,
+            6 => KEYWORDS::U16,
+            7 => KEYWORDS::U8,
+            8 => KEYWORDS::I8,
+            9 => KEYWORDS::BIT,
+            10 => KEYWORDS::F64,
+            11 => KEYWORDS::F32,
+            12 => KEYWORDS::FN,
+            13 => KEYWORDS::IF,
+            14 => KEYWORDS::ELSE,
+            15 => KEYWORDS::TYPE,
+            16 => KEYWORDS::THIS,
+            17 => KEYWORDS::NULL,
+            18 => KEYWORDS::UNDEF,
+            19 => KEYWORDS::CHAR,
+            20 => KEYWORDS::STRING,
+            21 => KEYWORDS::INLINE,
+            22 => KEYWORDS::STATIC,
+            23 => KEYWORDS::SWITCH,
+            24 => KEYWORDS::FOR,
+            25 => KEYWORDS::IN,
+            26 => KEYWORDS::OF,
+            27 => KEYWORDS::BREAK,
+            28 => KEYWORDS::ENUM,
+            29 => KEYWORDS::PUB,
+            30 => KEYWORDS::RETURN,
+            31 => KEYWORDS::ASYNC,
+            32 => KEYWORDS::AWAIT,
+            33 => KEYWORDS::BOX,
+            34 => KEYWORDS::TRAIT,
+            35 => KEYWORDS::PTR,
+            36 => KEYWORDS::MATCH,
+            37 => KEYWORDS::ADDR,
+            38 => KEYWORDS::LIST,
+            39 => KEYWORDS::VOL,
+            40 => KEYWORDS::TRUE,
+            41 => KEYWORDS::FALSE,
+            _ => {
+                panic!("no enum for u32");
+            }
+        }
+    }
+}
 #[derive(Debug, PartialEq)]
 pub enum OPS {
-    And, // &
-    Or, // |
-    Xor, // ^
+    And,    // &
+    Or,     // |
+    Xor,    // ^
     LShift, // <<
     RShift, // >>
-    Not, // ~
+    Not,    // ~
     // Assignment
-    AndAs, // &=
-    OrAs, // |=
-    XorAs, // ^=
+    AndAs,    // &=
+    OrAs,     // |=
+    XorAs,    // ^=
     LShiftAs, // <<=
     RShiftAs, // >>=
     // Logical
-    AndLog, // &&
-    OrLog, // ||
+    AndLog,      // &&
+    OrLog,       // ||
     NotEquality, // !=
-    Equality, // == 
-    NotLog, // !
+    Equality,    // ==
+    NotLog,      // !
     // Relational
-    Lt,
-    LtEq,
-    Gt,
-    GtEq,
+    Lt,   // <
+    LtEq, // <=
+    Gt,   // >
+    GtEq, // >=
     // Operators
-    Add,
-    Sub,
-    Mul,
-    Div,
-    Mod,
-    Inc,
-    Dec,
+    Add, // +
+    Sub, // -
+    Mul, // *
+    Div, // /
+    Mod, // %
+    Inc, // ++
+    Dec, // --
     // Operator and Assign
-    AddAs,
-    SubAs,
-    MulAs,
-    DivAs,
-    ModAs
+    AddAs, // +=
+    SubAs, // -=
+    MulAs, // *=
+    DivAs, // /=
+    ModAs, // %=
 }
 
 #[derive(Debug, PartialEq)]
@@ -204,7 +170,7 @@ pub enum TOKEN {
     Dollar,
     Pound,
     Operator(OPS),
-    Number([u8;8]),
+    Number([u8; 8]),
     Words(String),
     Keywords(KEYWORDS),
     Template(String),
@@ -228,11 +194,10 @@ pub enum TOKEN {
     CBrace,
     OBrace,
     Empty,
-    EOF
+    EOF,
 }
 
-
-fn parse_number(data: &str, vec: &mut Vec<(TOKEN, u32)>) -> () {
+fn parse_number(data: &str, vec: &mut Vec<(TOKEN, usize)>) -> () {
     let mut index = 0;
     let mut found = false;
     for c in data.chars() {
@@ -258,35 +223,13 @@ fn parse_number(data: &str, vec: &mut Vec<(TOKEN, u32)>) -> () {
     let val = &data[..index - 1];
 }
 
-fn ensure_word_to_end(data: &str) -> u32 {
-    let mut index = 0;
-    for c in data.chars() {
-        match c {
-            c if c.is_alphabetic() => {
-                index += 1;
-            }
-            c if c.is_digit(10) => {
-                index += 1;
-            }
-            '-' => {
-                index += 1;
-            }
-            '_' => index += 1,
-            _ => {
-                break;
-            }
-        }
-    }
-    return index;
-}
-
-fn seek_past_whitespace(data: &str) -> u32 {
+fn seek_past_whitespace(data: &str) -> usize {
     let mut index = 0;
     for c in data.chars() {
         match c {
             ' ' | '\n' | '\t' => {
                 index += 1;
-            },
+            }
             _ => {
                 break;
             }
@@ -295,7 +238,22 @@ fn seek_past_whitespace(data: &str) -> u32 {
     return index;
 }
 
-pub fn parse_char(data: &str, vec: &mut Vec<(TOKEN, u32)>, prev: &TOKEN) -> () {
+pub fn parse_op_fslash(data: &str, vec: &mut Vec<(TOKEN, usize)>) -> () {
+    let c = data.chars().next();
+    match c {
+        Some(c) => {
+            if c == '=' {
+                vec.push((TOKEN::Operator(OPS::DivAs), 2));
+            } else {
+                vec.push((TOKEN::Operator(OPS::Div), 1));
+            }
+        }
+        _ => {
+            vec.push((TOKEN::Operator(OPS::Div), 1));
+        }
+    }
+}
+pub fn parse_char(data: &str, vec: &mut Vec<(TOKEN, usize)>, prev: &TOKEN) -> () {
     let mut escape = false;
     let mut closed = false;
     let mut new_data: char = '\0';
@@ -356,14 +314,60 @@ pub fn parse_char(data: &str, vec: &mut Vec<(TOKEN, u32)>, prev: &TOKEN) -> () {
     return vec.push((TOKEN::Char(new_data), index - 1));
 }
 
-pub fn parse_word(data: &str, vec: &mut Vec<(TOKEN, u32)>, prev: &TOKEN) -> () {
-
+pub fn parse_word(data: &str, vec: &mut Vec<(TOKEN, usize)>) -> () {
+    let mut index = 0;
+    let mut found = false;
+    for c in data.chars() {
+        match c {
+            c if c.is_alphabetic() => {
+                index += 1;
+            }
+            c if c.is_digit(10) => {
+                index += 1;
+            }
+            '-' | '_' => {
+                index += 1;
+            }
+            _ => {
+                break;
+            }
+        }
+    }
+    for (i, _x) in KEYWORDS_L.iter().enumerate() {
+        if index == KEYWORDS_SIZE_L[i] {
+            if KEYWORDS_L[i].eq(&data[..index]) {
+                found = true;
+                vec.push((TOKEN::Keywords(KEYWORDS::from_usize(i)), KEYWORDS_SIZE_L[i]));
+            }
+        }
+    }
+    if !found {
+        vec.push((TOKEN::Words(String::from(&data[..index])), index));
+    }
 }
-pub fn parse_template(data: &str, vec: &mut Vec<(TOKEN, u32)>, prev: &TOKEN) -> () {
 
+pub fn parse_template(data: &str, vec: &mut Vec<(TOKEN, usize)>) -> () {
+    let mut index = 0;
+    let mut found = false;
+    for c in data.chars() {
+        match c {
+            c if c.is_alphabetic() => {
+                index += 1;
+            }
+            c if c.is_digit(10) => {
+                index += 1;
+            }
+            '-' | '_' => {
+                index += 1;
+            }
+            _ => {
+                break;
+            }
+        }
+    }
 }
 
-pub fn parse_quoted(data: &str, vec: &mut Vec<(TOKEN, u32)>, prev: &TOKEN) -> () {
+pub fn parse_quoted(data: &str, vec: &mut Vec<(TOKEN, usize)>, prev: &TOKEN) -> () {
     let mut escape = false;
     let mut closed = false;
     let mut new_data: String = "".to_string();
@@ -420,11 +424,10 @@ pub fn parse_quoted(data: &str, vec: &mut Vec<(TOKEN, u32)>, prev: &TOKEN) -> ()
     }
     if !closed {
         return vec.push((TOKEN::Error("expected closing \"".to_string()), index));
-    } 
+    }
 }
 
-
-pub fn tokenize(data: &str) -> Vec<(TOKEN, u32)> {
+pub fn tokenize(data: &str) -> Vec<(TOKEN, usize)> {
     let mut vec = Vec::new();
     let prev: TOKEN = TOKEN::Empty;
     for curr in data.chars() {
@@ -438,35 +441,36 @@ pub fn tokenize(data: &str) -> Vec<(TOKEN, u32)> {
             '"' => {
                 vec.push((TOKEN::DQuote, 1));
                 parse_quoted(&data[1..], &mut vec, &prev);
-            },
+            }
             '@' => vec.push((TOKEN::At, 1)),
             '%' => vec.push((TOKEN::Percent, 1)),
             '#' => vec.push((TOKEN::Pound, 1)),
             '/' => vec.push((TOKEN::FSlash, 1)),
-            '[' => vec.push((TOKEN::OArray,1)),
-            ']' => vec.push((TOKEN::CArray,1)),
+            '[' => vec.push((TOKEN::OArray, 1)),
+            ']' => vec.push((TOKEN::CArray, 1)),
             '\'' => {
                 vec.push((TOKEN::SQuote, 1));
                 parse_char(&data[1..], &mut vec, &prev);
-            },
+            }
             '\t' => vec.push((TOKEN::Empty, seek_past_whitespace(&data[1..]) + 1)),
             '\n' => vec.push((TOKEN::Empty, seek_past_whitespace(&data[1..]) + 1)),
             ' ' => vec.push((TOKEN::Empty, seek_past_whitespace(&data[1..]) + 1)),
-            // c if c.is_alphabetic() => parse_word(&data[1..], &mut vec),
+            c if c.is_alphabetic() => parse_word(&data[..], &mut vec),
             c if c.is_digit(10) => parse_number(&data[..], &mut vec),
-            _ => vec.push((TOKEN::Error("Invalid token found".to_string()), 1)), 
+            _ => vec.push((TOKEN::Error("Invalid token found".to_string()), 1)),
         }
     }
     return vec;
 }
+
 #[cfg(test)]
 mod tests {
-    use crate::{KEYWORDS_L, KEYWORDS_SIZE_L, tokenize, TOKEN, seek_past_whitespace};
+    use crate::*;
     #[test]
     fn test_correct_keywords_lengths() {
         assert_eq!(KEYWORDS_SIZE_L.len(), KEYWORDS_L.len());
-        for (i,x) in KEYWORDS_L.iter().enumerate() {
-            assert_eq!(x.len(), usize::from(KEYWORDS_SIZE_L[i]));
+        for (i, x) in KEYWORDS_L.iter().enumerate() {
+            assert_eq!(x.len(), KEYWORDS_SIZE_L[i]);
         }
     }
     #[test]
@@ -511,5 +515,40 @@ mod tests {
         assert_eq!(pre.0, TOKEN::Pound);
         assert_eq!(oarr.0, TOKEN::OArray);
         assert_eq!(carr.0, TOKEN::CArray);
+    }
+
+    #[test]
+    fn test_tokenize_keywords() {
+        for (i, x) in KEYWORDS_L.iter().enumerate() {
+            let vec = tokenize(&x);
+            assert_eq!(
+                vec.get(0).unwrap().0,
+                TOKEN::Keywords(KEYWORDS::from_usize(i))
+            );
+        }
+    }
+
+    #[test]
+    fn test_tokenize_multi_keywords() {
+        let vec = tokenize("const mut");
+        let one = vec.get(0).unwrap();
+        let two = vec.get(1).unwrap();
+
+        assert_eq!(one.0, TOKEN::Keywords(KEYWORDS::CONST));
+
+        assert_eq!(two.0, TOKEN::Keywords(KEYWORDS::MUT));
+    }
+
+    #[test]
+    fn test_tokenize_words() {
+        let vec = tokenize("hello worlds");
+        let one = vec.get(0).unwrap();
+        let two = vec.get(1).unwrap();
+
+        assert_eq!(one.0, TOKEN::Words("hello".to_string()));
+        assert_eq!(one.1, 5);
+
+        assert_eq!(two.0, TOKEN::Words("worlds".to_string()));
+        assert_eq!(two.1, 6);
     }
 }
