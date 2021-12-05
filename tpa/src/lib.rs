7c4b0e1df9edc3101cdc3ebfad6a9e109eadf3ab
Chris <chris.clark@coffeeb4code.com>
2021-12-04 20:08:43 -0600

    working easy variant

--- tpa/src/lib.rs
+++ tpa/src/lib.rs
@@ -158,86 +158,96 @@ pub enum KEYWORDS {
 }
 
 #[derive(Debug, PartialEq)]
+pub enum OPS {
+    And, // &
+    Or, // |
+    Xor, // ^
+    LShift, // <<
+    RShift, // >>
+    Not, // ~
+    // Assignment
+    AndAs, // &=
+    OrAs, // |=
+    XorAs, // ^=
+    LShiftAs, // <<=
+    RShiftAs, // >>=
+    // Logical
+    AndLog, // &&
+    OrLog, // ||
+    NotEquality, // !=
+    Equality, // == 
+    NotLog, // !
+    // Relational
+    Lt,
+    LtEq,
+    Gt,
+    GtEq,
+    // Operators
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Inc,
+    Dec,
+    // Operator and Assign
+    AddAs,
+    SubAs,
+    MulAs,
+    DivAs,
+    ModAs
+}
+
+#[derive(Debug, PartialEq)]
 pub enum TOKEN {
     Dot,
     Dollar,
     Pound,
+    Operator(OPS),
+    Number([u8;8]),
     Words(String),
     Keywords(KEYWORDS),
-    Number(String),
-    Operator(String),
+    Template(String),
     Char(char),
+    Error(String),
+    Percent,
     OArray,
     CArray,
-    SQuote,
-    DQuote,
     FSlash,
-    BSlash,
     Comma,
     Colon,
     SColon,
+    DQuote,
+    SQuote,
     Backtick,
     At,
     OCaret,
     CCaret,
-    UCaret,
     CParen,
     OParen,
     CBrace,
     OBrace,
     Empty,
-    End,
+    EOF
 }
 
-fn seek_past_newline_and_empty(data: &str) -> usize {
+
+fn parse_number(data: &str, vec: &mut Vec<(TOKEN, u32)>) -> () {
     let mut index = 0;
     let mut found = false;
     for c in data.chars() {
         match c {
-            ' ' => {}
-            '\n' => found = true,
-            '\t' => {}
-            _ => {
-                if found == true {
-                    break;
+            'x' | 'b' => {
+                if index != 1 {
+                    found = true;
+                    index -= 1;
                 }
             }
-        }
-        index += 1;
-    }
-    return index;
-}
-
-fn safe_ender(c: char) -> bool {
-    match c {
-            ' ' => true,
-            '\n' => true,
-            '\t' => true,
-            ')' => true,
-            '}' => true,
-            ';' => true,
-            ',' => true,
-            ']' => true,
-            _ => false,
-    }
-}
-
-fn parse_number(data: &str) -> Result<(TOKEN, usize), (TPAError, usize)> {
-    let mut index = 0;
-    let mut found = false;
-    for c in data.chars() {
-        match c {
-            c if safe_ender(c) => { found = true; }
-            'x' => {}
-            'b' => {}
             'f' => {}
             'l' => {}
             c if c.is_digit(10) => {}
             _ => {
-                return Err((
-                    TPAError(format!("invalid character found in number {}", c)),
-                    index,
-                ));
+                found = true;
             }
         }
         index += 1;
@@ -246,15 +256,12 @@ fn parse_number(data: &str) -> Result<(TOKEN, usize), (TPAError, usize)> {
         }
     }
     let val = &data[..index - 1];
-    return Ok((TOKEN::Number(val.parse().unwrap()), index - 1));
 }
 
-fn ensure_word_to_end(data: &str) -> Result<usize, (TPAError, usize)> {
+fn ensure_word_to_end(data: &str) -> u32 {
     let mut index = 0;
-    let mut found = false;
     for c in data.chars() {
         match c {
-            c if safe_ender(c) => { found = true; }
             c if c.is_alphabetic() => {
                 index += 1;
             }
@@ -266,45 +273,29 @@ fn ensure_word_to_end(data: &str) -> Result<usize, (TPAError, usize)> {
             }
             '_' => index += 1,
             _ => {
-                return Err((TPAError(format!("invalid character in word {}", c)), index));
-            }
-        }
-        if found {
-            break;
-        }
-    }
-    return Ok(index);
-}
-
-fn seek_past_whitespace(data: &str) -> usize {
-    let mut index = 0;
-    let mut found = false;
-    for c in data.chars() {
-        match c {
-            ' ' => {
-                index -= 1;
-                found = true
-            }
-            '\n' => {
-                index -= 1;
-                found = true
-            }
-            '\t' => {
-                index -= 1;
-                found = true
-            }
-            _ => {
-                if found == true {
-                    break;
-                }
-                index += 1;
+                break;
             }
         }
     }
     return index;
 }
 
-pub fn parse_char(data: &str) -> Result<(TOKEN, usize), (TPAError, usize)> {
+fn seek_past_whitespace(data: &str) -> u32 {
+    let mut index = 0;
+    for c in data.chars() {
+        match c {
+            ' ' | '\n' | '\t' => {
+                index += 1;
+            },
+            _ => {
+                break;
+            }
+        }
+    }
+    return index;
+}
+
+pub fn parse_char(data: &str, vec: &mut Vec<(TOKEN, u32)>, prev: &TOKEN) -> () {
     let mut escape = false;
     let mut closed = false;
     let mut new_data: char = '\0';
@@ -349,10 +340,7 @@ pub fn parse_char(data: &str) -> Result<(TOKEN, usize), (TPAError, usize)> {
                             new_data = '\u{0010}';
                         }
                         _ => {
-                            return Err((
-                                TPAError(format!("invalid escaped character {}", c)),
-                                index,
-                            ));
+                            break;
                         }
                     }
                 } else {
@@ -363,12 +351,19 @@ pub fn parse_char(data: &str) -> Result<(TOKEN, usize), (TPAError, usize)> {
         }
     }
     if !closed {
-        return Err((TPAError("expected closing \'".to_string()), index));
+        return vec.push((TOKEN::Error("expected closing \'".to_string()), index));
     }
-    return Ok((TOKEN::Char(new_data), index - 1));
+    return vec.push((TOKEN::Char(new_data), index - 1));
 }
 
-pub fn parse_quoted(data: &str) -> Result<(TOKEN, usize), (TPAError, usize)> {
+pub fn parse_word(data: &str, vec: &mut Vec<(TOKEN, u32)>, prev: &TOKEN) -> () {
+
+}
+pub fn parse_template(data: &str, vec: &mut Vec<(TOKEN, u32)>, prev: &TOKEN) -> () {
+
+}
+
+pub fn parse_quoted(data: &str, vec: &mut Vec<(TOKEN, u32)>, prev: &TOKEN) -> () {
     let mut escape = false;
     let mut closed = false;
     let mut new_data: String = "".to_string();
@@ -413,10 +408,7 @@ pub fn parse_quoted(data: &str) -> Result<(TOKEN, usize), (TPAError, usize)> {
                             new_data.push('\u{0010}');
                         }
                         _ => {
-                            return Err((
-                                TPAError(format!("invalid escape character {}", c)),
-                                index,
-                            ));
+                            break;
                         }
                     }
                 } else {
@@ -427,108 +419,97 @@ pub fn parse_quoted(data: &str) -> Result<(TOKEN, usize), (TPAError, usize)> {
         }
     }
     if !closed {
-        return Err((TPAError("expected closing \"".to_string()), index));
-    }
-    return Ok((TOKEN::Words(new_data), index - 1));
+        return vec.push((TOKEN::Error("expected closing \"".to_string()), index));
+    } 
 }
 
-pub fn parse_array(data: &str) -> Result<(TOKEN, usize), (TPAError, usize)> {
-    let mut vec: Vec<TOKEN> = vec![];
-    let mut closed = false;
-    let mut index = 0;
-    let mut iter = data.chars();
-    loop {
-        match iter.next() {
-            None => {
-                return Err((TPAError(format!("expected closing ]").to_string()), index));
-            }
-            Some(' ') => {
-                index += 1;
-            }
-            Some('\'') => {
-                let char_or = parse_char(&data[index..]);
-            }
-            Some(']') => {
-                closed = true;
-                index += 1;
-                break;
-            }
-            Some('"') => {
-                let quote_or = parse_quoted(&data[index..]);
-                match quote_or {
-                    Err(val) => return Err(val),
-                    Ok(val) => {
-                        vec.push(val.0);
-                        iter.advance_by(val.1 - 1);
-                        index += val.1;
-                    }
-                }
-            }
-            Some(val) => {
-                if val.is_alphabetic() {
-                    let word_or = make_ident_or_error(&data[index..], 6);
-                    match word_or {
-                        Err(v) => return Err(v),
-                        Ok(v) => {
-                            vec.push(v.0);
-                            iter.advance_by(v.1 - 1);
-                            index += v.1;
-                        }
-                    }
-                } else if val.is_digit(10) {
-                    let num_or = parse_number(&data[index..]);
-                    match num_or {
-                        Err(v) => return Err(v),
-                        Ok(v) => {
-                            vec.push(v.0);
-                            iter.advance_by(v.1 - 1);
-                            index += v.1;
-                        }
-                    }
-                } else {
-                    return Err((TPAError(format!("invalid character {:?}", iter)), index));
-                }
-            }
+
+pub fn tokenize(data: &str) -> Vec<(TOKEN, u32)> {
+    let mut vec = Vec::new();
+    let prev: TOKEN = TOKEN::Empty;
+    for curr in data.chars() {
+        match curr {
+            ';' => vec.push((TOKEN::SColon, 1)),
+            ':' => vec.push((TOKEN::Colon, 1)),
+            ',' => vec.push((TOKEN::Comma, 1)),
+            ')' => vec.push((TOKEN::CParen, 1)),
+            '(' => vec.push((TOKEN::OParen, 1)),
+            '$' => vec.push((TOKEN::Dollar, 1)),
+            '"' => {
+                vec.push((TOKEN::DQuote, 1));
+                parse_quoted(&data[1..], &mut vec, &prev);
+            },
+            '@' => vec.push((TOKEN::At, 1)),
+            '%' => vec.push((TOKEN::Percent, 1)),
+            '#' => vec.push((TOKEN::Pound, 1)),
+            '/' => vec.push((TOKEN::FSlash, 1)),
+            '[' => vec.push((TOKEN::OArray,1)),
+            ']' => vec.push((TOKEN::CArray,1)),
+            '\'' => {
+                vec.push((TOKEN::SQuote, 1));
+                parse_char(&data[1..], &mut vec, &prev);
+            },
+            '\t' => vec.push((TOKEN::Empty, seek_past_whitespace(&data[1..]) + 1)),
+            '\n' => vec.push((TOKEN::Empty, seek_past_whitespace(&data[1..]) + 1)),
+            ' ' => vec.push((TOKEN::Empty, seek_past_whitespace(&data[1..]) + 1)),
+            // c if c.is_alphabetic() => parse_word(&data[1..], &mut vec),
+            c if c.is_digit(10) => parse_number(&data[..], &mut vec),
+            _ => vec.push((TOKEN::Error("Invalid token found".to_string()), 1)), 
         }
     }
-    if !closed {
-        return Err((TPAError(format!("expected closing ]").to_string()), index));
-    }
-    return Ok((TOKEN::Array(vec), index - 1));
-}
-
-pub fn tokenize(data: &str) -> Result<(TOKEN, usize), (TPAError, usize)> {
-    let curr = data.chars().next().unwrap();
-    match curr {
-        ';' => Ok((TOKEN::End, seek_past_newline(data))),
-        ':' => make_ident_or_error(&data[1..], 2),
-        ',' => make_ident_or_error(&data[1..], 5),
-        ')' => Ok((TOKEN::CParen, 1)),
-        '(' => Ok((TOKEN::OParen, 1)),
-        '$' => make_ident_or_error(&data[1..], 0),
-        '"' => parse_quoted(&data[1..]),
-        '@' => make_ident_or_error(&data[1..], 1),
-        '%' => make_ident_or_error(&data[1..], 3),
-        '#' => make_ident_or_error(&data[1..], 4),
-        '/' => Ok((TOKEN::Comment, seek_past_newline(data))),
-        '[' => parse_array(&data[1..]),
-        '\'' => parse_char(&data[1..]),
-        '\t' => Ok((TOKEN::Empty, seek_past_whitespace(&data[1..]))),
-        '\n' => Ok((TOKEN::Empty, seek_past_whitespace(&data[1..]))),
-        ' ' => Ok((TOKEN::Empty, seek_past_whitespace(&data[1..]))),
-        c if c.is_alphabetic() => make_ident_or_error(&data[..], 6),
-        c if c.is_digit(10) => parse_number(&data[..]),
-        _ => Err((TPAError(format!("invalid token: {}", curr)), 0)),
-    }
+    return vec;
 }
 #[cfg(test)]
 mod tests {
-    use crate::{KEYWORDS_L, KEYWORDS_SIZE_L};
+    use crate::{KEYWORDS_L, KEYWORDS_SIZE_L, tokenize, TOKEN, seek_past_whitespace};
     #[test]
-    fn correct_keywords_lengths() {
+    fn test_correct_keywords_lengths() {
         assert_eq!(KEYWORDS_SIZE_L.len(), KEYWORDS_L.len());
         for (i,x) in KEYWORDS_L.iter().enumerate() {
             assert_eq!(x.len(), usize::from(KEYWORDS_SIZE_L[i]));
         }
+    }
+    #[test]
+    fn test_seek_past_whitespace_end() {
+        println!("thing");
+        let len = seek_past_whitespace("  \t\n");
+        assert_eq!(len, 4);
+    }
+    #[test]
+    fn test_seek_past_whitespace_in() {
+        let len = seek_past_whitespace(" \t\n\ty");
+        assert_eq!(len, 4);
+    }
+    #[test]
+    fn test_seek_past_whitespace_early() {
+        let len = seek_past_whitespace(" ");
+        assert_eq!(len, 1);
+    }
+    #[test]
+    fn test_tokenize_simple() {
+        let vec = tokenize(";:,)($@%#[]");
+        let semi = vec.get(0).unwrap();
+        let colon = vec.get(1).unwrap();
+        let comma = vec.get(2).unwrap();
+        let cparen = vec.get(3).unwrap();
+        let oparen = vec.get(4).unwrap();
+        let dollar = vec.get(5).unwrap();
+        let at = vec.get(6).unwrap();
+        let percent = vec.get(7).unwrap();
+        let pre = vec.get(8).unwrap();
+        let oarr = vec.get(9).unwrap();
+        let carr = vec.get(10).unwrap();
+
+        assert_eq!(semi.0, TOKEN::SColon);
+        assert_eq!(colon.0, TOKEN::Colon);
+        assert_eq!(comma.0, TOKEN::Comma);
+        assert_eq!(cparen.0, TOKEN::CParen);
+        assert_eq!(oparen.0, TOKEN::OParen);
+        assert_eq!(dollar.0, TOKEN::Dollar);
+        assert_eq!(at.0, TOKEN::At);
+        assert_eq!(percent.0, TOKEN::Percent);
+        assert_eq!(pre.0, TOKEN::Pound);
+        assert_eq!(oarr.0, TOKEN::OArray);
+        assert_eq!(carr.0, TOKEN::CArray);
     }
 }
