840271f48929e64c6f52f1ece7cfb56b7037f4ff
Chris <chris.clark@coffeeb4code.com>
2021-12-29 22:58:34 -0600

    now to seriously parse

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,3 +1,24 @@
+use logos::*;
+use tokens::*;
+use lexer::*;
+
+pub struct ParserSource<'source> {
+    lexer: LexerSource<'source>,
+    lineno: usize
+}
+
+impl<'source> ParserSource<'source> {
+    pub fn new(source: &'source str) -> Self {
+        Self {
+            lexer: LexerSource::new(source),
+            lineno: 0,
+        }
+    }
+    pub fn parse_declaration(&mut self) -> Result<(),_> {
+        
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
